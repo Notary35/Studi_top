62c3f32ae8c1598dee8cfc795ed313f204b3d7fc
Sergey Papilov <papilov.serhz@gmail.com>
2025-02-16 11:15:33 +0300

    less28 датакласс для итератора

--- lesson28.py
+++ lesson28.py
@@ -31,3 +31,35 @@ import json
 from typing import Iterator, List, Dict
 
 
+@dataclass
+class TranscriptionChunk:
+    text: str
+    float_start: float
+    float_end: float
+    str_start: str = field(init=False)
+    str_end: str = field(init=False)
+
+    def __post_init__(self):
+        self.str_start = self.float_start
+        self.str_end = self.float_end
+
+    def _int_to_str(self, time: float) -> str:
+        int_time = int(time)
+        hours = int_time // 3600
+        minutes = (int_time % 3600) // 60
+        seconds = int_time % 60
+        return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+
+
+def main():
+    test_data = {"timestamp": [0.0, 4.62], "text": "добро утра здравствуйте"}
+
+    transcription_chunks = TranscriptionChunk(
+        test_data["text"], test_data["timestamp"][0], test_data["timestamp"][1]
+    )
+    print(transcription_chunks)
+
+
+if __name__ == "__main__":
+    main()
+    exit()
