15a84f46870a56e4557fe3c8d2a88b34eb185d2f
Sergey Papilov <papilov.serhz@gmail.com>
2025-03-06 22:35:14 +0300

    less 16: Атрибуты класса

--- lesson_16/lesson_16.py
+++ lesson_16/lesson_16.py
@@ -17,6 +17,48 @@ class User:
 
 name = __name__
 print(name) # __main__
+print()
 user = User()
 print(user) # <__main__.User object at 0x0000020C2F68F190>
+print()
 print(type(user)) # <class '__main__.User'>
+print()
+
+srting_1 = str("Строка_1")
+srting_2 = str("Строка_2")
+srting_3 = str("Строка_3")
+print(type(srting_1)) # <class 'str'>
+print()
+print(srting_1)
+print()
+
+class Car:
+    mark: str = "BMW"
+    color: str = "Black"
+
+# Создаём атрибуты класса Car
+# Это разные объекты, они имеют разные адреса в ОЗУ
+car_1 = Car()
+car_2 = Car()
+
+print(car_1)
+print()
+print(type(car_1.mark))
+print()
+print(car_2)
+print()
+
+# У нас есть доступ к атрибутам класса через объект. Название атрибута
+print(f'Это автомобиль {car_1.mark} цветом {car_1.color}') # Это автомобиль BMW цветом Black
+print()
+print(car_2.color) # Black
+print()
+
+# Мы можем переопределить атрибуты если нужно
+car_2.color = "Red"
+car_2.options = ["ABS"] # добавляем новый атрибут вне класса, работать будет но не рекомендуется
+print(car_2.color) # Red
+print()
+print(car_2.options) # ['ABS']
+print()
+print(car_1.color) # Black
