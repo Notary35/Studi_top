b14480af85fec27f885ba14e3f689d85250124ed
Papilov Sergey <papilov.serhz@gmail.com>
2025-03-11 00:42:51 +0300

    less 17: валидация для set_salary приватного атрибута

--- lesson_17.py
+++ lesson_17.py
@@ -10,13 +10,28 @@ Lesson 17
 """
 # pylint: disable=all
 class Employee:
-    def __init__(self, name: str, age:int, salary: int) -> None:
+    def __init__(self, name: str, age:int, salary: int, threshold_percent_salary: int = 50) -> None:
         self.name = name
         self._age = age
         self.__salary = salary
+        self.__threshold_percent_salary: int = threshold_percent_salary
+    
+    def set_salary(self, value: int) -> None:
+        if type(value) != int:
+            raise ValueError('Зарплата должна быть числом')
+        if value < 0:
+            raise ValueError('Зарплата не может быть меньше 0')
+        # Проверим чтобы зарплата не колебалась более чем на 50%
+        if abs(self.__salary - value) > self.__salary * self.__threshold_percent_salary / 100:
+            raise ValueError(f'Зарплата не может быть изменена больше чем на {self.__threshold_percent_salary}%')
+        
+        self.__salary = value
         
     def get_salary(self) -> int:
         return self.__salary
-    
-    def set_salary(self, value: int) -> None:
-        self.__salary = value
+
+manager = Employee('John', 30, 100000)
+print(manager.get_salary())
+manager.set_salary(120000)
+print(manager.get_salary())
+
