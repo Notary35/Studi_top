f9bdd1572b33f40dbadaf9a294f896a2bc772e91
Papilov Sergey <papilov.serhz@gmail.com>
2025-03-11 01:19:51 +0300

    less 17: создание собственного исключения и обработка его в коде

--- lesson_17.py
+++ lesson_17.py
@@ -7,7 +7,21 @@ Lesson 17
 - Защищенные атрибуты
 - Защищенные методы
 - Доступы из вне
+
+Делаем 2 метода, которые называются одинаково
+Например это salary
+
+@property
+def salary - это для добычи данных из приватных атрибутов
+
+@salary.setter - мы делаем сеттер для конкретного атрибута
 """
+
+class SalaryException(ValueError):
+    """
+    Ошибка связанна с изменением в ЗП
+    """
+    pass
 # pylint: disable=all
 class Employee:
     def __init__(self, name: str, age:int, salary: int, threshold_percent_salary: int = 50) -> None:
@@ -16,22 +30,31 @@ class Employee:
         self.__salary = salary
         self.__threshold_percent_salary: int = threshold_percent_salary
     
-    def set_salary(self, value: int) -> None:
+    @property
+    def salary(self) -> int:
+        return self.__salary
+    
+    @salary.setter
+    def salary(self, value: int) -> None:
         if type(value) != int:
-            raise ValueError('Зарплата должна быть числом')
+            raise SalaryException('Зарплата должна быть числом')
         if value < 0:
-            raise ValueError('Зарплата не может быть меньше 0')
+            raise SalaryException('Зарплата не может быть меньше 0')
         # Проверим чтобы зарплата не колебалась более чем на 50%
         if abs(self.__salary - value) > self.__salary * self.__threshold_percent_salary / 100:
-            raise ValueError(f'Зарплата не может быть изменена больше чем на {self.__threshold_percent_salary}%')
+            raise SalaryException(f'Зарплата не может быть изменена больше чем на {self.__threshold_percent_salary}%')
         
         self.__salary = value
-        
-    def get_salary(self) -> int:
-        return self.__salary
 
 manager = Employee('John', 30, 100000)
-print(manager.get_salary())
-manager.set_salary(120000)
-print(manager.get_salary())
+print(manager.salary)
 
+while True:
+    new_salary = int(input('Введите новую зарплату: '))
+    try:
+        manager.salary = new_salary
+    except SalaryException as e:
+        print(e)
+    else:
+        print(f'Новая зарплата: {manager.salary}')
+        break
